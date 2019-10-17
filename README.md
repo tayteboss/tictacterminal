021d250de1836a08a1db635c119e254c80401db4
Tayte <boss.tayte@gmail.com>
2019-10-18 10:51:45 +1100

    testing github pages & README

--- README.md
+++ README.md
@@ -1,18 +1,44 @@
 # tic_tac_terminal
+**For those who enjoy a wholesome cli experience.**
 
-![alt text](placeholder.jpg)
+![tic_tac_toe screenshot](screenshot.jpg)
 
-### initiate gameplay here
+### Initiate Gameplay Here
 https://tayteboss.github.io/tictacterminal/
 
-## instructions
-Initiate the game by inputting the command ‘init’ in the terminal section. Players will take turns typing in their moves into the terminal. Use commands such as ‘top_left’ & ‘middle’ to signify your move. A small graphical hint to the right of the screen will help show the current board. Click the commands button towards the top right of the screen for a list of all valid commands.
+## Instructions
+Initiate the game by inputting the command ‘init’ in the terminal section. Players will take turns typing in their moves into the terminal. Use commands such as ‘top_left’ & ‘middle’ to signify your move. A small graphical hint to the right of the screen will help show the current board. Click the 'commands' button towards the top right of the screen for a list of all valid commands. First player to win 3 rounds wins the game.
 
-## concept
-lorem ipsum dolor sit amet
+## Concept
+My overall concept of the theme was the rethink the way we normally play the classic game tictactoe. After learning about the terminal and the CLI to navigate the computer, I wanted to emulate this experience for the game. 
 
-## technology used
-lorem ipsum dolor sit amet
+Throughout the design stage I wanted to remove as many of the graphical elements as possible and utilise typography as the crux of the gameplay experience. Taking references from the the terminal application, I've emulated the typefaces and the overall 'dark theme' to make the game look like a cli application.
 
-## current bugs
-lorem ipsum dolor sit amet
+The user experience of the game is what makes this project most interesting. Instead of relying on a tictactoe visual board, I wanted players to look at the history of the past moves and visually interpret the current board in their minds. This experience in turn leads to higher level thinking and more opportunity for mistakes during the game.
+
+## Technology & Concepts used
+* HTML 5
+* CSS 3
+* BEM CSS
+* SASS
+* Javascript
+* JQuery Animations
+* SVG Animations
+
+## Challenges
+### Structuring Data
+At the start of the build I struggled quite a bit with working out how to structure the data I would need to use throughout the program. I think this could have been alleviated if I wrote more puesocode before starting to code out structures. A lot of these issues were worked out through trial & error (failing fast).
+
+### Duplicating Code
+For someone that gets quite anxious about code duplication. I found myself constantly duplicating code for each player. By the end, I figured out some ways of creating generic functions for both players, however, there's still some instances in need of refactoring. 
+
+### Flow Control
+Flow Control seemed to be reoccuring problem throughout this process. Again I think these issues could have been alleviated if I wrote more puesodocode at the start of the project.
+
+### Variable Naming
+Towards the end of the build I struggled to work out what some variables were at a glance. I had to refactor quite a few variable names to make more sense of what the code was doing at a glance.
+
+## WIP
+* Not allowing any other commands beside 'init' at the begginning of the game.
+* Audio for the landing screen.
+* Refactoring Code.
--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-
-
 // THE GAME
 // recieve input a move (string) 'playerInputMove'
 // determine whose move it is (if 0 than playerOne if 1 than playerTwo)
 // store a variable of the players cell reference (1 = 'top-centre')
-// use these as an arguement for the rest of the methods
-
+// use these as an arguement for the rest of the method
 // trigger function to update the board -------------
 // loop through all cells
 // if the cellreference has a data-attribute-usage 'fill' - alert this move has already been made and stop remaining functions
@@ -19,7 +16,11 @@
 // if any of the arrays have winning combos (123, 147 etc)
 // stop the game and post the winner
 
-var commands = [
+
+
+
+
+var commands = [ // list of all valid commands
     'init',
     'clear',
     'top_left',
@@ -33,7 +34,7 @@ var commands = [
     'bottom_right',
 ]
 
-var board = { //cell references
+var board = { // cell references
     'top_left': 0,
     'middle_left': 1,
     'bottom_left': 2,
@@ -45,7 +46,7 @@ var board = { //cell references
     'bottom_right': 8
 }
 
-var game = { //storing all generic game details
+var game = { // storing all generic game details
     numMoves: 0,
     playerMoves: {
         playerOneMoves: [],
@@ -56,121 +57,27 @@ var game = { //storing all generic game details
     playerTwoScore: 0
 }
 
-var input = document.querySelector('.player-move-input') //variable for input field
-var playerTurnElem = document.querySelector('.player-turn') //variable for where playerturn text will go
-var cellsEl = document.querySelectorAll('.cells') //location cells elements
-var errorElem = document.querySelector('.error-field') //variable for where errors will be logged
+// GUI variables
+var input = document.querySelector('.player-move-input')
+var playerTurnElem = document.querySelector('.player-turn')
+var cellsEl = document.querySelectorAll('.cells')
+var errorElem = document.querySelector('.error-field') 
 var terminalSectionHistory = document.querySelector('.terminal__section--history')
 var terminalSection = document.querySelector('.terminal__section')
 var playerOneTotalScoreElem = document.querySelector('.player-one-score')
 var playerTwoTotalScoreElem = document.querySelector('.player-two-score')
-var counter = 5
 
-var playerOneMovesArr = game.playerMoves.playerOneMoves //variable for playerOneArray
-var playerTwoMovesArr = game.playerMoves.playerTwoMoves //variable for playerTwoArray
-var totalNumMoves = game.numMoves //variable for total moves played
-var currentTurn = game.currentPlayer //variable for currentTurn
+// Backend Variables
+var playerOneMovesArr = game.playerMoves.playerOneMoves
+var playerTwoMovesArr = game.playerMoves.playerTwoMoves
+var totalNumMoves = game.numMoves
+var currentTurn = game.currentPlayer
 var playerOneTotalScore = game.playerOneScore
 var playerTwoTotalScore = game.playerTwoScore
+var counter = 5
 
 
-
-
-function checkMatchWin() {
-    if (playerOneTotalScore === 2) {
-        matchWinCredit('playerOne')
-        clearRound()
-    } else if (playerTwoTotalScore === 2) {
-        matchWinCredit('playerTwo')
-        clearRound()
-    }
-}
-
-function winningState(currentPlayer) {
-    var newGame = setInterval(function(){
-    errorElem.textContent = `${currentPlayer} wins! new game in ${counter}secs`
-    counter--
-    if (counter === -1) {
-        clearGame();
-        clearInterval(newGame);
-    }
-    }, 1000);
-    counter = 5
-}
-
-function checkGameStatus() {
-
-    if (totalNumMoves === 9) { //testing whether it's a tie
-        errorElem.textContent = "notification: it's a tie - type 'clear' to play again"
-    }
-
-    var currentPlayerArr = [] //creating a temporary array for the current player
-
-    if (currentPlayer === 'playerOne') {
-        currentPlayerArr = playerOneMovesArr //current player array now = playerOneArr
-    } else {
-        currentPlayerArr = playerTwoMovesArr //current player array now = playerTwoArr
-    }
-
-    var winningCombos = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ]
-
-    winningCombos.forEach(function(winningCombosArr) {
-
-        var thereIsAWinner = winningCombosArr.every(function(val) {
-            return currentPlayerArr.indexOf(val) >= 0;
-        })
-
-        if (thereIsAWinner) {
-            if (currentPlayer === 'playerOne') { //updating total scores
-                playerOneTotalScore++
-                playerOneTotalScoreElem.textContent = playerOneTotalScore
-                checkMatchWin()
-                winningState(currentPlayer)
-            } else {
-                console.log(currentPlayer)
-                playerTwoTotalScore++
-                playerTwoTotalScoreElem.textContent = playerTwoTotalScore
-                checkMatchWin()
-                winningState(currentPlayer)
-            }
-        }
-    })
-}
-
-function updateGameStats(cellReference) {
-
-    if (currentPlayer === 'playerOne') {
-        playerOneMovesArr.push(cellReference) // pushing the players move into their array
-    } else {
-        playerTwoMovesArr.push(cellReference) // pushing the players move into their array
-    }
-    totalNumMoves++ //update the totalnummoves 
-}
-
-function updateGUI(chosenCellElem) { //function to update board
-
-    chosenCellElem.classList.add(`cell-fill-${currentPlayer}`)
-    chosenCellElem.setAttribute('data-cell-usage', 'filled')
-    
-    errorElem.textContent = "" //clearing any errors
-
-    $(terminalSectionHistory).append(terminalSectionHistory)
-    $(terminalSectionHistory).append($(terminalSection).clone())
-    $(terminalSectionHistory).slideDown('slow')
-
-    input.value = "" //clearing the input field
-}
-
-function changePlayer() {
+function changePlayer() { // finally change the player turn
     if (currentTurn === 0) { //playerOne Turn
         currentTurn = 1
         currentPlayer = 'playerTwo'
@@ -182,31 +89,127 @@ function changePlayer() {
     playerTurnElem.textContent = currentPlayer // update text for next persons turn
 }
 
-function checkIfLegalMove(playerMoveCommand) {
-    var cellReference = board[playerMoveCommand] //getting cell reference (1)
-    var chosenCellElem = cellsEl[cellReference] //cell chosen
-    var chosenCellAttrCellUsage = chosenCellElem.getAttribute('data-cell-usage')
+function winningState(currentPlayer) { // run a 5 sec countdown before clearing round and notify who won
+    var newGame = setInterval(function(){
+        errorElem.textContent = `${currentPlayer} wins! new game in ${counter}secs`
+        counter--
+        if (counter === -1) {
+            clearRound();
+            clearInterval(newGame);
+        }
+    }, 1000);
+    counter = 5 // reset counter to 5 for next round
+}
 
-    if (chosenCellAttrCellUsage === 'filled') {
+function checkGameWin() { // checking if game has been won. if so show player won credit screen and clear the game
+    if (playerOneTotalScore === 2) {
+        matchWinCredit('playerOne')
+        clearGame()
+    } else if (playerTwoTotalScore === 2) {
+        matchWinCredit('playerTwo')
+        clearGame()
+    }
+}
+
+function checkGameStatus() { // checks game status for winning round/game and if its a tie
+
+    if (totalNumMoves === 9) { // testing whether it's a tie
+        errorElem.textContent = "notification: it's a tie - type 'clear' to play again"
+    }
+
+    var currentPlayerArr = [] // creating a temporary array for the current player (generic winning testing)
+
+    if (currentPlayer === 'playerOne') { // making the current players move array the generic array
+        currentPlayerArr = playerOneMovesArr
+    } else {
+        currentPlayerArr = playerTwoMovesArr
+    }
+
+    var winningCombos = [ // list of all winning combinations
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    winningCombos.forEach(function(winningCombosArr) { // loops through each array
+
+        var thereIsAWinner = winningCombosArr.every(function(val) { // testing whether the current player array has the same numbers in one of the winning combo arrays - if so make 'thereisawinner' variable true
+            return currentPlayerArr.indexOf(val) >= 0;
+        })
+
+        if (thereIsAWinner) { // if thereisawinner is true, check which player is current and update their scores
+            if (currentPlayer === 'playerOne') {
+                playerOneTotalScore++
+                playerOneTotalScoreElem.textContent = playerOneTotalScore
+                checkGameWin() // check if the game has been won yet
+                winningState(currentPlayer) // get next round ready
+            } else {
+                console.log(currentPlayer)
+                playerTwoTotalScore++
+                playerTwoTotalScoreElem.textContent = playerTwoTotalScore
+                checkGameWin()  // check if the game has been won yet
+                winningState(currentPlayer)  // get next round ready
+            }
+        }
+    })
+}
+
+function updateGameStats(cellReference) { // updating all backend stats
+
+    if (currentPlayer === 'playerOne') {
+        playerOneMovesArr.push(cellReference) // pushing the players move into their array
+    } else {
+        playerTwoMovesArr.push(cellReference) // pushing the players move into their array
+    }
+    totalNumMoves++ //update the totalnummoves 
+}
+
+function updateGUI(chosenCellElem) { // updates all GUI 
+
+    input.placeholder = "enter command" // changing input.placeholder to enter command for the rest of the game
+
+    chosenCellElem.classList.add(`cell-fill-${currentPlayer}`) // adds styling to chosen cell (fill or stroke depending on player)
+    chosenCellElem.setAttribute('data-cell-usage', 'filled') // sets chosen cell attribute usage to filled for future reference
+    
+    errorElem.textContent = "" // clearing any errors (so errors don't get appending to terminal history)
+
+    // $(terminalSectionHistory).append(terminalSectionHistory)
+    $(terminalSectionHistory).append($(terminalSection).clone()) // append current move to the terminal history section make sure to clone it
+    $(terminalSectionHistory).slideDown('slow') // animate in the history section for the first time
+
+    input.value = "" //clearing the input field for next move
+}
+
+function checkIfLegalMove(playerMoveCommand) { // checks if move has already been made. if so, throw an error. if not, store that move for future ref
+
+    var cellReference = board[playerMoveCommand] // storing cell reference (1)
+    var chosenCellElem = cellsEl[cellReference] // storing cell chosen
+    var chosenCellAttrCellUsage = chosenCellElem.getAttribute('data-cell-usage') // variable for chosen cell data attribute
+
+    if (chosenCellAttrCellUsage === 'filled') { // if the chosen cell already has a data attribute of filled - throw error. if not, move on with control flow
         errorElem.textContent = 'typeError: move already played'
     } else {
-        updateGUI(chosenCellElem) //call updateboard function
-        updateGameStats(cellReference) //call update game stats function
+        updateGUI(chosenCellElem)
+        updateGameStats(cellReference)
         checkGameStatus(playerMoveCommand)
         changePlayer()
 
     }
 }
 
-function clearGame() {
-    //update GUI stuff
+function clearRound() { // runs once round is complete - clears most GUI & some Backend stats
     totalNumMoves = 0
     playerOneMovesArr = []
     playerTwoMovesArr = []
 
-    playerTurnElem.textContent = 'playerOne' //clearing playerturn text
-    errorElem.textContent = '' //clearing any errors
-    input.value = '' //clearing the input field
+    playerTurnElem.textContent = 'playerOne'
+    errorElem.textContent = ''
+    input.value = ''
 
     cellsEl.forEach(function (e) {
         e.classList.remove(`cell-fill-playerOne`)
@@ -226,8 +229,7 @@ function clearGame() {
 
 }
 
-function clearRound() {
-    //update GUI stuff
+function clearGame() { // runs once game is complete - clears all GUI & Backend stats
     totalNumMoves = 0
     playerOneMovesArr = []
     playerTwoMovesArr = []
@@ -235,10 +237,9 @@ function clearRound() {
     playerOneTotalScoreElem.textContent = playerOneTotalScore
     playerTwoTotalScore = 0
     playerTwoTotalScoreElem.textContent = playerTwoTotalScore
-
-    playerTurnElem.textContent = 'playerOne' //clearing playerturn text
-    errorElem.textContent = '' //clearing any errors
-    input.value = '' //clearing the input field
+    playerTurnElem.textContent = 'playerOne'
+    errorElem.textContent = ''
+    input.value = ''
 
     cellsEl.forEach(function (e) {
         e.classList.remove(`cell-fill-playerOne`)
@@ -260,67 +261,45 @@ function clearRound() {
 
 }
 
-function checkCommand() {
+function checkCommand() { // checks the input value to decide whether to continue the control flow or reply with an error
 
-    var playerMoveCommand = input.value // storing player move input value (middle)
+    var playerMoveCommand = input.value // storing the players input value
 
-    input.placeholder = "enter command"
-
-    if (playerMoveCommand === 'init') {
-        playerTurnElem.textContent = currentPlayer
+    if (playerMoveCommand === 'init') { // if init is input - collapses the instructions section & displays playerturn
         expandTerminal()
-        return
-    }
-
-    if (playerMoveCommand === 'clear') {
-        clearGame()
         playerTurnElem.textContent = currentPlayer
         return
     }
 
-    if (commands.includes(playerMoveCommand)) {
+    if (playerMoveCommand === 'clear') { // if clear is input - it runs clearRound function
+        clearRound()
+        playerTurnElem.textContent = currentPlayer
+        return
+    }
+
+    if (commands.includes(playerMoveCommand)) { // if input is one of the commands it moves on through to the next stage of the control flow
         checkIfLegalMove(playerMoveCommand)
-    } else {
+    } else { // if input doesn't match any of the commands it throws an error
         errorElem.textContent = 'typeError: invalid command'
     }
 }
 
-//while inputting value wait for enter key to be pressed
-input.addEventListener('keypress', function (e) {
+
+input.addEventListener('keypress', function (e) { // game waits for the enter key to pressed while the user is focused on the input field
     if (e.key === 'Enter') {
         checkCommand()
     }
 });
 
-function newGame() {
+function newGame() { // on refresh this function runs to get everything setup for the game
 
-    cellsEl.forEach(function (e) {
-        e.classList.remove(`cell-fill-playerOne`)
-        e.classList.remove(`cell-fill-playerTwo`)
-        e.setAttribute('data-cell-usage', 'clear')
-    })
-
-    input.placeholder = "type 'init' to begin" //clearing the input field
+    input.placeholder = "type 'init' to begin"
 
     currentPlayer = 'playerOne'
     currentTurn = 0
-
-    var terminalSectionHistoryChildren = terminalSectionHistory.childNodes; 
-    while (terminalSectionHistoryChildren.length > 0) {
-        terminalSectionHistoryChildren[0].remove()
-    }
 
 }
 
 newGame()
 
 
-
-
-// reset game completely after winner reaches 2 wins
-// only start the game once 'init' has been input
-// play a sound on landing screen
-// get explanation of iffi on winningstate function -->
-
-// bugs
-// when playertwo ones. the counter goes to playerone
